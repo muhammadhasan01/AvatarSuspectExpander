80d814c9ab92de60b6780d75d3601f947da0ad80
Muhammad Hasan <muhammadhasan50@gmail.com>
2020-06-22 08:28:39 +0700

    Updates

---
+++ precache-manifest.8a75351bed5e981be4e149b4b4b97c62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "983cdbb9821e389e0adfa889eb4df0a1",
+    "url": "/AvatarSuspectExpander/index.html"
+  },
+  {
+    "revision": "9a09427b8caebe1a0868",
+    "url": "/AvatarSuspectExpander/static/css/main.e6c12f86.chunk.css"
+  },
+  {
+    "revision": "20339fad41199362ef75",
+    "url": "/AvatarSuspectExpander/static/js/2.698d0b01.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/AvatarSuspectExpander/static/js/2.698d0b01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a09427b8caebe1a0868",
+    "url": "/AvatarSuspectExpander/static/js/main.25164b32.chunk.js"
+  },
+  {
+    "revision": "2a806b3c55d9a6ec4fdc",
+    "url": "/AvatarSuspectExpander/static/js/runtime-main.a82236d2.js"
+  }
+]);
