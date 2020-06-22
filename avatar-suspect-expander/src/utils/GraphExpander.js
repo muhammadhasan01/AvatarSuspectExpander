686455ac403036a64679ebdeab05ccb3c1bcec1f
Muhammad Hasan <muhammadhasan50@gmail.com>
2020-06-22 21:13:45 +0700

    Added GraphExpander Util

---
+++ avatar-suspect-expander/src/utils/GraphExpander.js
@@ -0,0 +1,27 @@
+import ColorElement from './ColorElement';
+
+const GraphExpander = (graphData, id, friends) => {
+    let { data, mapData, mapLinks } = graphData;
+    friends.forEach((friend) => {
+        if (mapData.has(friend.id) === false) {
+            mapData.set(friend.id, { name: friend.name, element: friend.element });
+            data.nodes.push({
+                id: friend.id, 
+                color: ColorElement.get(friend.element)
+            });
+        }
+        if (mapLinks.has((id, friend.id)) === false) {
+            mapLinks.set((id, friend.id), true);
+            mapLinks.set((friend.id, id), true);
+            data.links.push({ 
+                source: id,
+                target: friend.id,
+                color: 'black'
+            });
+        }
+    });
+
+    return { data, mapData, mapLinks };
+}
+
+export default GraphExpander;
